ba780351535ce19333f85de3eac762ed66d1c540
Antonio Stassi <stassiant@outlook.it>
2024-11-07 19:49:07 +0100

    Generic components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <footer class="my-4">
+    <div class="container">
+      <div class="row">
+          <div class="col">
+            COPYRIGHTS BY SPLENDIDI: <a target="_blank"  href="https://github.com/antoniostassi">Antonio Stassi</a> and <a target="_blank" href="https://github.com/AleScla">Alessandro Sclafani</a>
+          </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+ 
+}
+</script>
+
+<template>
+  <header class="py-4">
+    
+    <img src="https://www.pokemoncenter.com/site/binaries/content/gallery/bloomreach/site-logo/logo.svg" alt="pokemon-center">
+    <h5 class="d-inline-block ms-2"> Fake</h5>
+    
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+  header{
+    text-align:center;
+    img{
+      max-width:400px;
+    }
+  }
+</style>
