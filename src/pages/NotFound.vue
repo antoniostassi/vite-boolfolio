39b708148408a18f93ad01ff28c8be1cf993eca8
Antonio Stassi <stassiant@outlook.it>
2024-11-07 19:49:35 +0100

    Not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,34 @@
+<script>
+import AppHeader from '../components/AppHeader.vue';
+
+import AppFooter from '../components/AppFooter.vue';
+
+
+export default {
+  data() {
+    
+  },
+  components: {
+    AppHeader,
+
+    AppFooter
+  },
+}
+</script>
+
+
+<template>
+    <AppHeader />
+        
+    <div class="container">
+        <h1 class="fs-5">
+            404: Pokemon non trovato
+        </h1>
+    </div>
+
+    <AppFooter />
+</template>
+  
+<style lang="scss" scoped>
+
+</style>
