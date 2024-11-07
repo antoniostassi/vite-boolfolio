1323e7f9516001f3db32add72b5cc18af75aac00
Antonio Stassi <stassiant@outlook.it>
2024-11-07 19:48:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+
+
+export default {
+  data() {
+    
+  },
+  // 2) Dichiarazione del componente
+
+}
+</script>
+
+<template>
+  <div class="bg-grey">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+@use 'assets/scss/main' as *;
+// Import all of Bootstrap's CSS
+@import "bootstrap/scss/bootstrap";
+  .bg-grey{
+    background-color:lightgrey;
+    height:100vh;
+  }
+</style>
